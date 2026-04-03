839e698527ccefe6e2766d0b962a207d2651609e
suhyeon-gwon <yhus110@snu.ac.kr>
2026-04-03 13:17:19 +0900

    first commit

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
