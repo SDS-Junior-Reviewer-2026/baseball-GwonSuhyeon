0ecf694d6cd2dc889702fb343c8712613af0ff80
suhyeon-gwon <yhus110@snu.ac.kr>
2026-04-03 13:34:30 +0900

    [feature] 46p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -30,6 +30,6 @@ public class GameTest {
         assertIllegalArgument(null);
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
-
+        assertIllegalArgument("12s");
     }
 }
